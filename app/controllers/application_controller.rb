df5691ad5eb9f3cd0fef1892c7fe0fb9d2b6b2f6
Jerry Swan <swanj21@gmail.com>
2018-05-28 11:34:18 -0500

    Modified the application controller text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, world!"
+    render html: "Welcome to my Sample App!"
   end
 end
